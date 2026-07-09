7ed7fdd9685e8e692578f1ebe036b37f2e554daf
Kiruthicraj49 <24243049@drngpit.ac.in>
2026-07-09 15:07:22 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 15:07:22
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
