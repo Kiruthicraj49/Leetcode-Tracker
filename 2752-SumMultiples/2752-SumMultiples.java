7fba6e2069c0cb73e5f3e3bc647e9274621df23c
Kiruthicraj49 <24243049@drngpit.ac.in>
2026-07-09 15:05:39 +0530

    Create 2752-SumMultiples

---
+++ 2752-SumMultiples/2752-SumMultiples.java
@@ -0,0 +1,12 @@
+// Last updated: 09/07/2026, 15:05:39
+class Solution {
+    public int sumOfMultiples(int n) {
+        int sum=0;
+        for (int i=1;i<=n;i++){
+            if(i%3==0 || i%5==0 || i%7==0){
+                sum=sum+i;
+            }
+        }
+        return sum;
+    }
+}
