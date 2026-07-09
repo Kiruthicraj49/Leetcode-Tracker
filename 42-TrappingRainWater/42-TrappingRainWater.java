74aa57b427945055a2e38920e1a716a233deba5a
Kiruthicraj49 <24243049@drngpit.ac.in>
2026-07-09 15:08:57 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -0,0 +1,24 @@
+// Last updated: 09/07/2026, 15:08:57
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        if (n == 0) return 0;
+        int[] pre = new int[n];
+        int[] suf = new int[n];
+        pre[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            pre[i] = Math.max(pre[i - 1], height[i]);
+        }
+
+        suf[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suf[i] = Math.max(suf[i + 1], height[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += Math.min(pre[i], suf[i]) - height[i];
+        }
+        return ans;
+    }
+}
