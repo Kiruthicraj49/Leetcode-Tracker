2f74a0e85c755a39018b3dd1a576765f9be51efb
Kiruthicraj49 <24243049@drngpit.ac.in>
2026-07-09 15:09:08 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.java
@@ -0,0 +1,30 @@
+// Last updated: 09/07/2026, 15:09:08
+class Solution {
+    static int convert(char s) {
+        switch(s) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+    public int romanToInt(String s) {
+        int n = s.length();
+        int ans = convert(s.charAt(n - 1));
+        for(int i = n - 2; i >= 0; i--) {
+            int a = convert(s.charAt(i));
+            int b = convert(s.charAt(i + 1));
+            if(a < b) {
+                ans = ans - a;
+            }
+            else {
+                ans = ans + a;
+            }
+        }
+        return ans;
+    }
+}
