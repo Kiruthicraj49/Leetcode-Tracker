fa636b9055f2524c5e51a06f26ad921bb6fd6c02
Kiruthicraj49 <24243049@drngpit.ac.in>
2026-07-09 15:05:27 +0530

    Create 3379-ScoreOfAString

---
+++ 3379-ScoreOfAString/3379-ScoreOfAString.java
@@ -0,0 +1,11 @@
+// Last updated: 09/07/2026, 15:05:27
+class Solution {
+    public int scoreOfString(String s) {
+        int sum=0;
+        for(int i=0;i<s.length()-1;i++){
+            int diff = Math.abs(s.charAt(i) - s.charAt(i+1));
+            sum=sum + diff;
+        }
+        return sum;
+    }
+}
