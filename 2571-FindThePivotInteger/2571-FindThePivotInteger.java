ad73191756e2ede296821ccbb3a12649e5f793e5
Kiruthicraj49 <24243049@drngpit.ac.in>
2026-07-09 15:05:52 +0530

    Create 2571-FindThePivotInteger

---
+++ 2571-FindThePivotInteger/2571-FindThePivotInteger.java
@@ -0,0 +1,18 @@
+// Last updated: 09/07/2026, 15:05:52
+class Solution {
+    public int pivotInteger(int n) {
+        for(int x=1;x<=n;x++){
+            int ls=0,rs=0;
+            for (int i=1;i<=x;i++){
+                ls=ls+i;
+            }
+            for(int j=x;j<=n;j++){
+                rs=rs+j;
+            }
+            if(ls==rs){
+                return x;
+            }
+        }
+        return -1;
+    }
+}
