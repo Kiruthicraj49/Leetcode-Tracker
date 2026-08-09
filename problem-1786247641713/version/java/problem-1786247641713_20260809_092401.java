cca23341e7e12faeaddde0da30ad1fe7d5919975
Kiruthicraj49 <24243049@drngpit.ac.in>
2026-08-09 09:24:02 +0530

    Create

---
+++ problem-1786247641713/version/java/problem-1786247641713_20260809_092401.java
@@ -0,0 +1,20 @@
+// Last updated: 09/08/2026, 09:24:01
+1class Solution {
+2    public double minPrice(int[] prices, int[] discounts) {
+3        Arrays.sort(prices);
+4        Arrays.sort(discounts);
+5        int n=prices.length;
+6        int m=discounts.length;
+7        double totalcost=0.0;
+8        int k=Math.min(n,m);
+9        for(int i=0;i<k;i++){
+10            int price = prices[n-1-i];
+11            int discount=discounts[m-1-i];
+12            totalcost=totalcost+ price*(100.0-discount)/100.0;
+13        }
+14        for(int i=0;i<n-k;i++){
+15            totalcost=totalcost+prices[i];
+16        }
+17        return totalcost;
+18    }
+19}
