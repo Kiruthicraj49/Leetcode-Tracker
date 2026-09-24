ed20a8fa2313919514e694b60e0898f0b39fcb6a
Kiruthicraj49 <24243049@drngpit.ac.in>
2026-09-24 10:15:57 +0530

    Create 152-MaximumProductSubarray

---
+++ 152-MaximumProductSubarray/version/java/152-MaximumProductSubarray_20260924_101559.java
@@ -0,0 +1,17 @@
+// Last updated: 24/09/2026, 10:15:59
+1class Solution {
+2    public int maxProduct(int[] nums) {
+3        int n=nums.length;
+4        int max=nums[0];
+5        for(int i=0;i<n;i++){
+6            int pro=1;
+7            for(int j=i;j<n;j++){
+8                pro = pro * nums[j];
+9                if(pro>max){
+10                    max=pro;
+11                }
+12            }
+13        }
+14        return max;
+15    }
+16}
